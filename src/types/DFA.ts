12735a78cc07cef0587292c6f0db76bc7e2ba8f7
Sten Laane <laanesten@gmail.com>
2021-02-20 14:02:27 +0200

    Add DFA serialization and deserialization

--- src/types/DFA.ts
+++ src/types/DFA.ts
@@ -11,3 +11,11 @@ export interface State {
     name: string;
     transitions: Transitions;
 }
+
+export interface SerializedDfa {
+    states: string[];
+    transitions: [string, string, string][]; // fromState, symbol, toState
+    startingState: string;
+    finalStates: string[];
+    alphabet: string[];
+}
