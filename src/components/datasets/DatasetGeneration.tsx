35acb4449fa70462c799fc7c7952bc7ab8f75a6f
Sten Laane <sten@laane.xyz>
2021-03-04 06:02:23 +0200

    Add ability to customize state prefix in dataset generation

--- src/components/datasets/DatasetGeneration.tsx
+++ src/components/datasets/DatasetGeneration.tsx
@@ -1,15 +1,48 @@
 import { useState } from "react";
+import { default as Tooltip } from "react-tooltip";
 import AlphabetInput from "../input/dfa/AlphabetInput";
 import DatasetGenerator from "./DatasetGenerator";
 
 export default function DatasetGeneration() {
     const [alphabet, setAlphabet] = useState<string[]>(["0", "1"]);
-    // todo: let user set stateprefix
+    const [statePrefix, setStatePrefix] = useState<string>("q");
     return (
         <div className={"page-container"}>
             <h2>Dataset Generation</h2>
             <AlphabetInput alphabet={alphabet} callback={setAlphabet} />
-            <DatasetGenerator alphabet={alphabet} statePrefix={"q"} />
+            <label htmlFor={"statePrefix"}>
+                State prefix
+                <span className={"info-tooltip"} data-tip data-for="state-prefix-help">
+                    ?
+                </span>
+            </label>
+            <Tooltip
+                place={"top"}
+                type={"info"}
+                id="state-prefix-help"
+                effect={"solid"}
+                multiline={true}
+            >
+                <span>
+                    The state prefix is prepended to name of every state in the generated DFA.
+                    <br />
+                    For example, if the prefix is "q", then the states in the DFA will be "q0",
+                    "q1", etc.
+                    <br />
+                    This is useful to customize if you wish to generate and compare different DFAs
+                    and want to differentiate their states with different prefixes.
+                </span>
+            </Tooltip>
+            <input
+                className={"state-prefix"}
+                name={"statePrefix"}
+                type={"text"}
+                value={statePrefix}
+                onChange={(event) => {
+                    setStatePrefix(event.target.value);
+                }}
+            />
+            <DatasetGenerator alphabet={alphabet} statePrefix={statePrefix} />
         </div>
     );
 }
