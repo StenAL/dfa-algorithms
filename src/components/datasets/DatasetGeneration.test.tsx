544de9973120eee11654d292cb74cd786bcc5e18
Sten Laane <sten@laane.xyz>
2021-03-04 06:02:23 +0200

    Add ability to customize state prefix in dataset generation

--- src/components/datasets/DatasetGeneration.test.tsx
+++ src/components/datasets/DatasetGeneration.test.tsx
@@ -4,11 +4,13 @@ import AlphabetInput from "../input/dfa/AlphabetInput";
 import DatasetGeneration from "./DatasetGeneration";
 import DatasetGenerator from "./DatasetGenerator";
 
-it("passes alphabet to generator", function () {
+it("passes alphabet and state prefix to generator", function () {
     const wrapper = mount(<DatasetGeneration />);
+    wrapper.find(".state-prefix").simulate("change", { target: { value: "xxx" } });
     const alphabetInput = wrapper.find(AlphabetInput);
     act(() => alphabetInput.props().callback(["a", "b"]));
     wrapper.setProps({});
     expect(wrapper.find(DatasetGenerator).length).toBe(1);
     expect(wrapper.find(DatasetGenerator).props().alphabet).toEqual(["a", "b"]);
+    expect(wrapper.find(DatasetGenerator).props().statePrefix).toEqual("xxx");
 });
