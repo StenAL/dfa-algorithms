9fe7e17c63b2cfe0811288f8f427fe2c5c5a98b6
Sten Laane <sten@laane.xyz>
2021-03-14 23:37:18 +0200

    Add random DFAs used for comparing algorithm run-times

--- src/algorithm/localTesting.ts
+++ src/algorithm/localTesting.ts
@@ -4,12 +4,36 @@
 //
 // sprawlingDatasetGenerator(20, ["a", "b", "c"], 5);
 
-import { getPrettyDfaString } from "../util/util";
-import { linearDatasetGenerator } from "./data/datasetGenerator";
-import { exampleDfa1, exampleDfa2 } from "./data/exampleData";
+import * as fs from "fs";
+import { DFA } from "../types/DFA";
+import { deserializeDfa, getPrettyDfaString, serializeDfa } from "../util/util";
+import {
+    linearDatasetGenerator,
+    randomDatasetGenerator,
+    sprawlingDatasetGenerator,
+} from "./data/datasetGenerator";
+import { HopcroftAlgorithmImpl } from "./HopcroftAlgorithm";
+import { NearlyLinearAlgorithmImpl } from "./NearlyLinearAlgorithm";
 import { TableFillingAlgorithmImpl } from "./TableFillingAlgorithm";
 
-const a = linearDatasetGenerator(10, ["0", "1"], 1);
-const b = new TableFillingAlgorithmImpl(a);
-b.run();
-// console.log(b)
+const dfaA = linearDatasetGenerator(100, ["0", "1"], 10, "q");
+const dfaB = linearDatasetGenerator(100, ["0", "1"], 10, "p");
+//
+// const serializedA = serializeDfa(dfaA);
+// const serializedB = serializeDfa(dfaB);
+// fs.writeFile("src/algorithm/randomA.txt", JSON.stringify(serializedA), () => {});
+// fs.writeFile("src/algorithm/randomB.txt", JSON.stringify(serializedB), () => {});
+
+// const fileA = fs.readFileSync("src/algorithm/randomA.txt", "utf-8");
+// const dfaA: DFA = deserializeDfa(JSON.parse(fileA));
+//
+// const fileB = fs.readFileSync("src/algorithm/randomB.txt", "utf-8");
+// const dfaB: DFA = deserializeDfa(JSON.parse(fileB));
+
+const iterations = 5;
+for (let i = 0; i < iterations; i++) {
+    const algorithm = new TableFillingAlgorithmImpl(dfaA, dfaB);
+    console.time("algo");
+    algorithm.run();
+    console.timeEnd("algo");
+}
